96ee647f9c793489a9ceaa95c2c836b08cf8bf5f
ShallowGreen123 <2608653986@qq.com>
2022-08-07 23:31:35 +0800

    stu: Test the basic funtions of SDRAM

---
+++ Core/Inc/main.h
@@ -0,0 +1,80 @@
+/* USER CODE BEGIN Header */
+/**
+ ******************************************************************************
+ * @file           : main.h
+ * @brief          : Header for main.c file.
+ *                   This file contains the common defines of the application.
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2022 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+  typedef uint32_t u32;
+  typedef uint16_t u16;
+  typedef uint8_t u8;
+  typedef __IO uint32_t vu32;
+  typedef __IO uint16_t vu16;
+  typedef __IO uint8_t vu8;
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define Led1_Pin GPIO_PIN_0
+#define Led1_GPIO_Port GPIOB
+#define Led0_Pin GPIO_PIN_1
+#define Led0_GPIO_Port GPIOB
+/* USER CODE BEGIN Private defines */
+#define LED0_ON HAL_GPIO_WritePin(Led0_GPIO_Port, Led0_Pin, GPIO_PIN_RESET)
+#define LED0_OFF HAL_GPIO_WritePin(Led0_GPIO_Port, Led0_Pin, GPIO_PIN_SET)
+#define LED1_ON HAL_GPIO_WritePin(Led1_GPIO_Port, Led1_Pin, GPIO_PIN_RESET)
+#define LED1_OFF HAL_GPIO_WritePin(Led1_GPIO_Port, Led1_Pin, GPIO_PIN_SET)
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
---
+++ MDK-ARM/F429IGT6_test/startup_stm32f429xx.d
@@ -0,0 +1,1 @@
+f429igt6_test\startup_stm32f429xx.o: startup_stm32f429xx.s
